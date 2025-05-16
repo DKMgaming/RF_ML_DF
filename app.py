aaff20e05f134b51fc5dfbe673afc670895d364e
DKMgaming <53993378+DKMgaming@users.noreply.github.com>
2025-05-16 11:02:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -384,7 +384,7 @@ with tab2:
                         #folium.Marker([lat, lon], tooltip="Điểm giao cắt", icon=folium.Icon(color='green')).add_to(m)
                         #st.write(f"Tọa độ nguồn phát tần số {freq} MHz là {lat:.4f},{lon:.4f}...")
             with st.container():
-                st_folium(m, width=700, height=500, returned_objects=[])
+                st_folium(m, width=1300, height=500, returned_objects=[])
             #st_folium(m, width=1300, height=500)
         else:
             with st.form("input_form"):
