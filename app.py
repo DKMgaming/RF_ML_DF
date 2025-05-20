6b3801314ace7d70005ef67c87341854387c5ca0
DKMgaming <53993378+DKMgaming@users.noreply.github.com>
2025-05-20 11:03:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -316,7 +316,7 @@ with tab2:
     uploaded_model = st.file_uploader("📂 Tải mô hình đã huấn luyện (.joblib)", type=["joblib"])
     if uploaded_model:
         with open("distance_model.joblib", "rb") as f:
-        model = joblib.load(f)
+            model = joblib.load(f)
         model = joblib.load(uploaded_model)
 
         uploaded_excel = st.file_uploader("📄 Hoặc tải file Excel chứa thông tin các trạm thu", type=["xlsx"])
